5f05c02d797bb60234a56ec9325d383ca9c67538
courtlandcampo <courtland.campo@ufl.edu>
2022-03-27 17:34:37 -0400

    API Key fixed

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ form.onsubmit = function(functionEvent) {
   functionEvent.preventDefault()
   var usersInput = input.value
   if(!usersInput) return
-  fetch('https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=51e49b626dc2924e1eeab8c2d1ddb3d5&q=' + usersInput)
+  fetch('https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=5e8fb7436a0e6f1c8b62aad1d8797764&q=' + usersInput)
   .then(function(resultJson) {
       return resultJson.json();
   })
